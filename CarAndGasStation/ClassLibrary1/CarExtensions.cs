325a432e1af0ac369087ed3afbea4797ed59e2be
Oksav <vasko.pecanov@live.com>
2019-04-05 16:26:30 +0200

    No Summary

---
+++ CarAndGasStation/ClassLibrary1/CarExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   public class CarExtensions
+    {
+        public static void Drive(Car c, string x)
+        {
+            Random rnd = new Random();
+            int addFuel = rnd.Next(10, 50);
+            Console.WriteLine($"Driving to {x}. And take some {addFuel} litres fuel. ");
+        }
+    }
+}
---
+++ CarAndGasStation/ClassLibrary1/GasStation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary1
+{
+   public static class GasStation
+    {
+        public static void Refill(Car c)
+        {
+            Random rnd = new Random();
+            int addFuel = rnd.Next(10, 50);
+            c.Fuel += addFuel;
+            Console.WriteLine($"The car was refueld {c.Fuel} litres.");
+        }
+
+        public static void PumpUpTires(Car c)
+        {
+            c.IsDrivable = true;
+            Console.WriteLine("The car now is in good state");
+        }
+        
+    }
+}
